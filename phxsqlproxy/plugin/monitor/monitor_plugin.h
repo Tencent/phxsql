64042d38dbd01579921fec75f28cebf3883d2876
wodesuck <wodesuck@gmail.com>
2016-09-28 15:57:35 +0800

    Add comments for moniter plugin

--- phxsqlproxy/plugin/monitor/monitor_plugin.h
+++ phxsqlproxy/plugin/monitor/monitor_plugin.h
@@ -23,35 +23,82 @@ class MonitorPlugin {
     virtual void SetConfig(phxsqlproxy::PHXSqlProxyConfig * config, phxsqlproxy::WorkerConfig_t * worker_config);
 
  public:
+    // Accept connection fail
     virtual void AcceptFail();
+
+    // Accept a new connection
     virtual void AcceptSuccess();
+
+    // Out of io routine (to handle received connection)
     virtual void OutOfConn();
+
+    // Resume a io routine (handle a connection)
     virtual void ResumeRoutine();
 
+    // An io routine is recycled by io routine manager
     virtual void RecycleRoutine();
+
+    // An io routine is allocated by io routine manager
     virtual void AllocRoutine();
+
+    // Connect to mysql or another phxsqlproxy
     virtual void ConnectDest();
+
+    // Connect to mysql or another phxsqlproxy fail
     virtual void ConnectDestFail();
+
+    // Connect to mysql with fd, fd < 0 indicating failure
     virtual void ConnectMysqlSvr(int fd);
+
+    // Cost cost_ms ms to connect mysql
     virtual void ConnectMysqlSvrRunTime(uint64_t cost_ms);
+
+    // Write write_size bytes to mysql or another phxsqlproxy (passthrough), including failed ones
     virtual void WriteNetwork(uint32_t write_size);
+
+    // Write write_size bytes to mysql or another phxsqlproxy fail
     virtual void WriteNetworkFail(uint32_t write_size);
+
+    // Cost cost_ms to execute a request
     virtual void RequestExecuteCost(uint64_t cost_ms);
+
+    // Io routine Receipt a read event but read fail
     virtual void ReadNetworkFail();
+
+    // Epoll cost cost_ms ms in io routine (including time for io)
     virtual void Epocost(uint64_t cost_ms);
+
+    // Receive byte_size bytes from destination (mysql or another phxsqlproxy)
     virtual void ReceiveByteFromConnectDestSvr(uint32_t byte_size);
+
+    // Receive byte_size bytes from client (mysql client or another phxsqlproxy)
     virtual void ReceiveByteFromMysqlClient(uint32_t byte_size);
+
+    // Receive byte_size bytes from mysql
     virtual void ReceiveByteFromMysql(uint32_t byte_size);
+
+    // Client connect master through read only port, reject
     virtual void ConnectSlavePortOnMaster();
 
+    // Mysql query cost cost_ms ms
     virtual void MysqlQueryCost(uint64_t cost_ms);
+
+    // Receive a response packet from destination (mysql or another phxsqlproxy)
     virtual void DestSvrRespPacket();
+
+    // Receive a request packet from client (mysql client or another phxsqlproxy)
     virtual void ClientReqPacket();
+
+    // Write byte_size bytes to mysql
     virtual void MysqlClientConnect(uint32_t byte_size);
 
+    // Get an expired master from phxbinlogsvr (no available master)
     virtual void CheckMasterInvalid();
+
+    // Get master from phxbinlogsvr fail
     virtual void GetMasterInBinLogFail();
 
+    // Routine_count io routines are working
     virtual void WorkingRoutine(int32_t routine_count);
 };
 
