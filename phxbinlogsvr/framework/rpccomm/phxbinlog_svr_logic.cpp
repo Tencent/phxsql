0ed61479c88ff8afdba99027d6cfa160c1994e2f
Daniel Zhang <wodesuck@gmail.com>
2016-10-25 19:34:42 +0800

    fix build

--- phxbinlogsvr/framework/rpccomm/phxbinlog_svr_logic.cpp
+++ phxbinlogsvr/framework/rpccomm/phxbinlog_svr_logic.cpp
@@ -1,5 +1,4 @@
 /*
-/*
 	Tencent is pleased to support the open source community by making PhxSQL available.
 	Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
 	Licensed under the GNU General Public License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
