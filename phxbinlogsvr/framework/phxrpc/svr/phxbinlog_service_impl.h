ef8e6213bd82af9cf1a9720ad03640857562df77
taohe <taohex@gmail.com>
2017-12-08 18:16:33 +0800

    fix phxrpc

--- phxbinlogsvr/framework/phxrpc/svr/phxbinlog_service_impl.h
+++ phxbinlogsvr/framework/phxrpc/svr/phxbinlog_service_impl.h
@@ -2,9 +2,9 @@
 	Tencent is pleased to support the open source community by making PhxSQL available.
 	Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
 	Licensed under the GNU General Public License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-	
+
 	https://opensource.org/licenses/GPL-2.0
-	
+
 	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
@@ -28,7 +28,7 @@ class PhxbinlogServiceImpl : public PhxbinlogService {
     PhxbinlogServiceImpl(ServiceArgs_t * service_args);
     virtual ~PhxbinlogServiceImpl();
 
-    virtual int PHXEcho(const google::protobuf::StringValue & req, google::protobuf::StringValue *resp);
+    virtual int PhxEcho(const google::protobuf::StringValue & req, google::protobuf::StringValue *resp);
 
     virtual int SendBinLog(const google::protobuf::BytesValue & req, google::protobuf::Empty *resp);
 
